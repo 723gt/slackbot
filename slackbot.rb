6e3f7933f81e4b41aa131e0aeb9ef3e32d856259
723 <rb.natsumi@gmail.com>
2017-02-18 21:46:29 +0900

    🆕 1st commit

---
+++ slackbot.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'slack'
+require 'json'
+#TOKEN = 'xoxb-143472026245-Yu2SJ0KRfJdx6uyE1WS1p89v'
+token = nil
+File.open("token.json") do |file|
+  token = JSON.load(file)
+end
+
+Slack.configure do |c|
+  c.token = token["token"] 
+end
+
+Slack.chat_postMessage(channel: 'botfarm', text: 'hoge',username: 'starplatinum')
+
+=begin
+client = Slack.realtime
+
+client.on :hello do
+  client.message channel: 'botfarm', text: 'hello'
+end
+=end
